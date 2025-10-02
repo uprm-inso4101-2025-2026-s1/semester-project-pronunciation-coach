582ebb99347c2d762c826bc8883d305db633be66
klrskevin <kevin.ruiz16@upr.edu>
2025-10-02 04:46:49 -0800

    Home Page Sections

--- app/lib/features/homescreen/pages/home_page.dart
+++ app/lib/features/homescreen/pages/home_page.dart
@@ -2,40 +2,44 @@ import 'package:app/core/constants/colors.dart';
 import 'package:app/features/homescreen/widgets/user_info_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:app/features/homescreen/widgets/home_sections.dart';
 
-class HomeScreen extends StatefulWidget { 
-    const HomeScreen({super.key});
-    
-    @override
-    State<HomeScreen> createState() => _HomeScreenState();
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-    @override 
-    Widget build(BuildContext context) {
-        return Scaffold(
-            backgroundColor: AppColors.background,
-            appBar: AppBar(
-                title: const Text(
-                    'Home Page',
-                    style: TextStyle(fontWeight: FontWeight.w600),
-                ),
-                backgroundColor: AppColors.cardBackground,
-                foregroundColor: AppColors.textPrimary,
-                elevation: 0,
-                systemOverlayStyle: SystemUiOverlayStyle.dark,
-                automaticallyImplyLeading: false,
-            ),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const UserInfoBox(
-                  name: "Maria Hernandez",
-                  avatarURL: "https://www.applesfromny.com/wp-content/uploads/2020/06/SnapdragonNEW.png",
-                  proficiencyLevel: "Intermediate Student",
-                ),
-              ]              
-            )
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        title: const Text(
+          'Home Page',
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+        backgroundColor: AppColors.cardBackground,
+        foregroundColor: AppColors.textPrimary,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const UserInfoBox(
+            name: "Maria Hernandez",
+            avatarURL:
+                "https://www.applesfromny.com/wp-content/uploads/2020/06/SnapdragonNEW.png",
+            proficiencyLevel: "Intermediate Student",
+          ),
+          const SizedBox(height: 16), // Espacio
+          const HomeSections(),
+        ],
+      ),
+    );
+  }
 }
