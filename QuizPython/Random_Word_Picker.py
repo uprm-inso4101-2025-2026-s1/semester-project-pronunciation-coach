1d81af1542edd52404b52eb42b89e90605bd01ec
uziellopez7 <uziel.lopez@upr.edu>
2025-10-21 11:45:40 -0400

    Added early version of TTS (provided by @Joy-Martinez), updated quiz to use TTS, and updated comments to reflect the changes and clarify better some lines of code

--- QuizPython/Random_Word_Picker.py
+++ QuizPython/Random_Word_Picker.py
@@ -9,14 +9,14 @@ nltk.download("brown")
 
 #Returns a completely random English word
 def Get_Random_English_Word() -> str:
-    word_list = [w for w in brown.words() if (len(w) > 3) and (w.isalpha())]
+    word_list = [w for w in brown.words() if (len(w) > 3) and (w.isalpha())] #Filters out any words that have symbols and words with 3 letters or less
     return random.choice(word_list).lower()
 
 #Returns a top nth commonly used word (the higher the)
 def Get_Random_Top_nth_Common_Word(n:int) -> str:
-    frequency = Counter(w.lower() for w in brown.words() if w.isalpha())
+    frequency = Counter(w.lower() for w in brown.words() if w.isalpha()) #Creates a dictionary with a word:frequency
     #word_list = set(words.words())
-    common_words = [w for w, c in frequency.most_common(n) if w.isalpha()]
+    common_words = [w for w, c in frequency.most_common(n) if w.isalpha()] #Filter out words that're not with the top nth commonly used words
     return random.choice(common_words)
 
 #print(Get_Random_English_Word())
