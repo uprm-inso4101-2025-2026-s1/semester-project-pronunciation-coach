a3186ec7a849f5b40a1b721086bb6f8746bddd8d
uziellopez7 <uziel.lopez@upr.edu>
2025-10-21 02:08:55 -0400

    Added an early version of TSS and set it up to take the randomly selected words

--- QuizPython/Quiz_with_user_input.py
+++ QuizPython/Quiz_with_user_input.py
@@ -3,6 +3,7 @@ import os
 import random
 from User_input_for_quiz import *
 from Random_Word_Picker import *
+from I_want_to_finish_this_shit import *
 #import nltk
 #nltk.download('cmudict')
 #from nltk.corpus import cmudict
@@ -46,114 +47,117 @@ def Get_Word_Syllables(seletected_word:str) -> str:
     return data[seletected_word]["Syllables"]
 
 #Function where it generates wrong answers for the problem by randomizing each syllable position and characters
-def Generate_Evil_Words(IPA_word:str) -> list:
+def Generate_Evil_Words(word:str) -> list:
+    print(word)
     
     wrong_answers = [] #List where it carries the wrong answers after they've been horribly disfigured
+    wrong_answers.append([word])
 
     #Primary loop, where the magic happens! Three times to generate 3 erroneous words
     for i in range(3):
-        IPA_word_temp = IPA_word #Copies the IPA_word into a temp variable in other to modify the IPA word without affecting the original one
+        #print(i)
+        word_temp = word[1:-1] #Copies the IPA_word into a temp variable in other to modify the IPA word without affecting the original one
 
-        #Secondary loop, where it takes the IPA word and move the characters and syllables around
-        #It's in a while loop to make sure the erroneous IPA word is NOT the same as the correct IPA word (very useful for shorter words since they're more likely to cause repeats)
-        #If the finished erroneous IPA word is the same as the correct IPA word, it restarts the process again
-        #If not, the erroneous IPA is added into the list of wrong answers (wrong_answers)
-        while(IPA_word_temp == IPA_word):
-            #Prepares the IPA word for mixing and matching
-            IPA_word_temp = IPA_word.strip("/")
-            IPA_word_temp = IPA_word_temp.strip()
-            IPA_word_list = IPA_word_temp.split(" ")
+        word_temp = list(word_temp)
+        random.shuffle(word_temp)
+        word_temp = "".join(word_temp)
 
-            IPA_word_list_temp = []
-
-            #Tertiary loop, where each IPA word's syllable's characters are shuffled around
-            for syllable in IPA_word_list:
-                syllable = list(syllable)
-                random.shuffle(syllable)
-                syllable = "".join(syllable)+(" ")
-                IPA_word_list_temp.append(syllable)
-            
-            IPA_word_list = IPA_word_list_temp.copy()
-
-            #Finally, it shuffles the syllables around
-            random.shuffle(IPA_word_list)
-            IPA_word_temp = ("/ ")+("".join(IPA_word_list))+("/")
-
-        wrong_answers.append(IPA_word_temp)
+        word_temp = word[0] + word_temp + word[-1]
+        
+        print(word_temp)
+        wrong_answers.append([word_temp])
+        print(wrong_answers)
 
     return wrong_answers
 
 #------Start------
+def main():
+    #Sets up the initial values for the quiz
+    #words = Words_for_Quiz() #Place where the user inputs the amount and which words they'd like to practice
+    #Code from issue #90 (User input for quiz)
+    num_quizes = 0 #Tracks how many quizes the user's taken
+    num_incorrect = 0 #How many quizes the user got incorrect
+    continue_quiz = True
+    is_correct = True
 
-#Sets up the initial values for the quiz
-words = Words_for_Quiz() #Place where the user inputs the amount and which words they'd like to practice
-#Code from issue #90 (User input for quiz)
-num_quizes = 0 #Tracks how many quizes the user's taken
-num_incorrect = 0 #How many quizes the user got incorrect
-is_correct = True
+    #print(Get_Random_English_Word())
+    #print(Get_Random_Top_nth_Common_Word(1000))
 
-#The quiz will continue until all the words inputted are used
-while ((len(words) != 0) or (not is_correct)):
-    num_quizes += 1
+    #The quiz will continue until all the words inputted are used
+    while (continue_quiz):
+        num_quizes += 1
 
-    #Checks whether the answer is correct.
-    #If true, it will go into the next word of the list.
-    #If false, it will not go into the next word in the list, as it will continue asking the user the same word until it's correct
-    if is_correct:
-        word = Select_Word(words)
-        try:
-            wordIPA = Get_Word_IPA(word)
-        except:
-            print("Couldn't find word's IPA! Removing from queue...")
-            num_quizes -= 1
-            continue
+        #Checks whether the answer is correct.
+        #If true, it will go into the next word of the list.
+        #If false, it will not go into the next word in the list, as it will continue asking the user the same word until it's correct
+        if is_correct:
+            word = Get_Random_English_Word()
         
-        options = Generate_Evil_Words(wordIPA)
-        options.append(wordIPA)
+        options = Generate_Evil_Words(word)
+        #Shuffles the answers around
+        random.shuffle(options)
+
+        #Actual quiz stuff
+        print("Select the correct pronunciation for " + word)
+        print("1. " + options[0][0])
+        print("2. " + options[1][0])
+        print("3. " + options[2][0])
+        print("4. " + options[3][0])
+
+        TTS_Speak(options[0])
+        TTS_Speak(options[1])
+        TTS_Speak(options[2])
+        TTS_Speak(options[3])
+
+        #Prompts the user to select a valid answer
+        while True:
+            try:
+                answer = int(input("Answer: ")) - 1
+                if (-1 < answer) and (answer < 4):
+                    break
+                #If an answer is NOT valid, it will through an error to go into the except block
+                else:
+                    0/0 #PERFECT way to cause an error :P
+
+            #The except block, tells the user their answer is invalid to regives them to retry 
+            except:
+                print("Please select a valid answer")
     
-    #Shuffles the answers around
-    random.shuffle(options)
+        #Checks if the user selected the correct answer
+        #If the answer is correct, it praises the user and restarts the quiz with the next word
+        #If the answer is incorrect, it gives the user feedback and restarts the quiz with the same word
+   
+        if options[answer][0] == word:
+            print("Correct!")
+            is_correct = True
+        
+            while True:
+                user_answer = input("Continue? [y/n] ")
 
-    #Actual quiz stuff
-    print("Select the correct pronunciation for " + word + ".")
-    print("1. " + options[0])
-    print("2. " + options[1])
-    print("3. " + options[2])
-    print("4. " + options[3])
+                if(user_answer == "n"):
+                    continue_quiz = False
+                    break
 
-    #Prompts the user to select a valid answer
-    while True:
-        try:
-            answer = int(input("Answer: ")) - 1
-            if (-1 < answer) and (answer < 4):
-                break
-            #If an answer is NOT valid, it will through an error to go into the except block
-            else:
-                0/0 #PERFECT way to cause an error :P
+                elif(user_answer == "y"):
+                    continue_quiz = True
+                    break
+                else:
+                    print("Select a valid answer!")
 
-        #The except block, tells the user their answer is invalid to regives them to retry 
-        except:
-            print("Please select a valid answer")
-    
-    #Checks if the user selected the correct answer
-    #If the answer is correct, it praises the user and restarts the quiz with the next word
-    #If the answer is incorrect, it gives the user feedback and restarts the quiz with the same word
-    if options[answer] == wordIPA:
-        print("Correct!")
-        is_correct = True
-    else:
-        num_incorrect += 1
-        print("Incorrect!")
-        is_correct = False
-    #This loop will continue until ALL words have been aswered correctly
+        else:
+            num_incorrect += 1
+            print("Incorrect!")
+            is_correct = False
 
-print("Number of Correct Answers: " + str(num_quizes-num_incorrect) + "/" + str(num_quizes))
+        #This loop will continue until ALL words have been aswered correctly
+
+    print("Number of Correct Answers: " + str(num_quizes-num_incorrect) + "/" + str(num_quizes))
 
 #Note: The code can be modified to fit in UI and interactive elements later down the line.
 #Note: This quiz only works if the user inputs: "destiny","defenestration","thou","coke", or "bass"
 #This can changed in the future once a database or a word bank is properly implemented/added (which should contain, at least, English words with their IPAs)
 
-
+main()
 
 
 
--- QuizPython/Random_Word_Picker.py
+++ QuizPython/Random_Word_Picker.py
@@ -5,11 +5,12 @@ from collections import Counter
 import random
 
 nltk.download("brown")
-nltk.download("words")
+
 
 #Returns a completely random English word
 def Get_Random_English_Word() -> str:
-    return random.choice(brown.words())
+    word_list = [w for w in brown.words() if (len(w) > 3) and (w.isalpha())]
+    return random.choice(word_list).lower()
 
 #Returns a top nth commonly used word (the higher the)
 def Get_Random_Top_nth_Common_Word(n:int) -> str:
